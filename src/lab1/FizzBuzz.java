1da033d2f0fd31ac224a776c2ea5d3d544738650
williamyao27 <william.yao.27@gmail.com>
2025-09-07 21:57:39 -0400

    extracted print logic into helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -11,29 +11,32 @@ class FizzBuzz {
 
     private static void doFizzBuzz() {
         for (int i = 1; i < 100; i++) {
+            printFizzBuzz(i);
+        }
+    }
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+    private static void printFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-                System.out.println("Fizz Buzz");
+            System.out.println("Fizz Buzz");
 
-            } else if (divisibleBy3) {
+        } else if (divisibleBy3) {
 
-                System.out.println("Fizz");
+            System.out.println("Fizz");
 
-            } else if (divisibleBy5) {
+        } else if (divisibleBy5) {
 
-                System.out.println("Buzz");
+            System.out.println("Buzz");
 
-            } else {
+        } else {
 
-                System.out.println(i);
+            System.out.println(i);
 
-            }
         }
     }
 }
